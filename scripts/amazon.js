f1b0163a9b26bd378d1e91aac4526adff88eac77
FavasUmmer <favasummer06@gmail.com>
2025-09-05 09:11:57 +0530

    added msg and select box added

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,7 +1,7 @@
 
 let productsHTML='';
- products.forEach((product)=>{
- productsHTML +=` <div class="product-container">
+products.forEach((product)=>{
+productsHTML +=` <div class="product-container">
           <div class="product-image-container">
             <img alt="image cant seen" class=" product-image"src="${product.image}">
           </div>
@@ -23,7 +23,7 @@ let productsHTML='';
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="quantitySelect">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -39,7 +39,7 @@ let productsHTML='';
 
           <div class="product-spacer"></div>
 
-          <div class="added-to-cart">
+          <div class="added-to-cart ">
             <img alt="image cant seen" src="images/icons/checkmark.png">
             Added
           </div>
@@ -48,36 +48,55 @@ let productsHTML='';
             Add to Cart
           </button>
         </div>`;
- })
+})
  
- document.querySelector('.js-products-grid').innerHTML=productsHTML;
+document.querySelector('.js-products-grid').innerHTML=productsHTML;
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
-  button.addEventListener('click',() => {
+button.addEventListener('click',() => {
     let machingItem;
+    const productContainer = button.closest('.product-container');
+    const selectBox = productContainer.querySelector('.quantitySelect');
     const buttenId=button.dataset.productId;
     cart.forEach((item)=>{
        if(buttenId===item.productId){
         machingItem=item;
        }
     });
+    valueselectBox=parseInt(selectBox.value)
+ 
 
-    if(machingItem){
-      machingItem.quantity +=1;
-    }else{
-      cart.push({
-        productId:buttenId,
-        quantity:1
-      });
+    if(valueselectBox>1){
+      if(machingItem){
+        machingItem.quantity +=valueselectBox;
+      }else{
+        cart.push({
+          productId:buttenId,
+          quantity:valueselectBox
+        });
+      }
+    }else{    
+        if(machingItem){
+          machingItem.quantity +=1;
+        }else{
+          cart.push({
+            productId:buttenId,
+            quantity:1
+          });
+        }
     }
+    let added =  button.previousElementSibling;
+    added.style.opacity = 5;
+    setTimeout(() => {
+       added.style.opacity = 0;
+      }, 1000);
     let cartQuantity=0;
  
-cart.forEach((item)=>{
-  cartQuantity +=item.quantity;
-});
-console.log(cartQuantity);
+    cart.forEach((item)=>{
+      cartQuantity +=item.quantity;
+    });
+    console.log(cartQuantity);
     console.log(cart)
-    document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
-    
+    document.querySelector('.js-cart-quantity').innerHTML=cartQuantity; 
   });
 
 });
