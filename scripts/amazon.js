bb1d7b068e3c14b22a31949840be6e138b0eed43
FavasUmmer <favasummer06@gmail.com>
2025-09-05 10:47:10 +0530

    creat a modules for the cart and products js files

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,5 @@
-
+import { cart } from '../data/cart.js';
+import { products } from '../data/products.js';
 let productsHTML='';
 products.forEach((product)=>{
 productsHTML +=` <div class="product-container">
@@ -62,7 +63,7 @@ button.addEventListener('click',() => {
         machingItem=item;
        }
     });
-    valueselectBox=parseInt(selectBox.value)
+    let valueselectBox=parseInt(selectBox.value)
  
 
     if(valueselectBox>1){
