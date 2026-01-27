4df4850c00ede5e09af953e98a27c14818aeaa9c
FavasUmmer <favasummer06@gmail.com>
2026-01-27 14:02:26 +0530

    using dayjs in checkout corrected date

--- data/cart.js
+++ data/cart.js
@@ -2,14 +2,13 @@ export let cart = JSON.parse(localStorage.getItem('cart'));
 if(!cart){
  cart = [{
     productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity:1
+    quantity:1,
+    deliveryOptionId:'1'
 },
 {
     productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity:2
-},{
-    productId:'83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
-    quantity:1
+    quantity:2,
+    deliveryOptionId:'2'
 }
 ];
 }
@@ -24,7 +23,8 @@ function saveToStorage(){
         }else{
           cart.push({
             productId:buttenId,
-            quantity:valueselectBox
+            quantity:valueselectBox,
+            deliveryOptionId:'1'
           });
         }
       }else{    
@@ -33,13 +33,15 @@ function saveToStorage(){
           }else{
             cart.push({
               productId:buttenId,
-              quantity:1
+              quantity:1,
+              deliveryOptionId:'1'
+
             });
           }
       }
       saveToStorage();
     }
-    export function removeFromCart(productId){
+    export function removeFromCart(productId){ 
       let newCart =[];
       cart.forEach((cartItem)=>{
         if(cartItem.productId !== productId){
